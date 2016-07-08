4e8e67ed225b65ec0132a03a79e19a5d52f54164
vzhz <vehanus@gmail.com>
2016-07-08 11:05:25 -0400

    PEP8'd the script, removed silliest affirmations

--- randline.py
+++ randline.py
@@ -1,11 +1,10 @@
-import random, sys
+import random
+import sys
 
 def main():
-	lines = open(sys.argv[1]).readlines()
-	print (lines[random.randrange(len(lines))])
+    lines = open(sys.argv[1]).readlines()
+    print(lines[random.randrange(len(lines))])
 
 
 if __name__ == "__main__":
-	main()
-#from http://stackoverflow.com/questions/448005/whats-an-easy-way-to-read-random-line-from-a-file-in-unix-command-line/448021#448021
-#note: written in python3
+    main()
